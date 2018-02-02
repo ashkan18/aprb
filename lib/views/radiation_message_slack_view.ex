11cca18657e01aadb6725af40d66faed3915dc28
ashkan <ashkan@artsymail.com>
2018-02-02 12:06:51 -0500

    Ignore open and click events

--- lib/views/radiation_message_slack_view.ex
+++ lib/views/radiation_message_slack_view.ex
@@ -1,23 +1,25 @@
 defmodule Aprb.Views.RadiationMessageSlackView do
   import Aprb.ViewHelper
   def render(event) do
-    %{
-      text: ":sadbot: #{event["verb"]} event for #{radiation_link(event["object"]["link"])}",
-      attachments: [%{
-                      fields: [
-                        %{
-                          title: "Recipient Name",
-                          value: "#{event["properties"]["to_name"]}",
-                          short: true
-                        },
-                        %{
-                          title: "Recipient Email",
-                          value: "#{event["properties"]["to_email_address"]}",
-                          short: true
-                        }
-                      ]
-                    }],
-      unfurl_links: false
-    }
+    if event["verb"] in ["bounce", "spamreport"] do
+      %{
+        text: ":sadbot: #{event["verb"]} event for #{radiation_link(event["object"]["link"])}",
+        attachments: [%{
+                        fields: [
+                          %{
+                            title: "Recipient Name",
+                            value: "#{event["properties"]["to_name"]}",
+                            short: true
+                          },
+                          %{
+                            title: "Recipient Email",
+                            value: "#{event["properties"]["to_email_address"]}",
+                            short: true
+                          }
+                        ]
+                      }],
+        unfurl_links: false
+      }
+    end
   end
 end
